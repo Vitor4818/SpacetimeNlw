215662016132e629f4cfd6a966d7e4489180d4ae
Vitor4818 <vitorg.martins4818@gmail.com>
2023-06-09 19:41:31 -0300

    first commit

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,10 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NW da{' '}
+      <a target="blank" rel="noreferrer" href="https://rocketseat.com.br">
+        Rocketseat
+      </a>
+    </div>
+  )
+}
